e6fa22d44054279227db31446bcf8f0640ab16bd
Igor Ognichenko <igor.ognichenko@fireblink.com>
2018-08-02 16:13:27 +0300

    fix tests

---
+++ test/helpers/MockA6sRailwayExternalFileStationHandler.ts
@@ -0,0 +1,23 @@
+import {A6s_Railway_ExternalFile_StationHandler} from '../../lib/stations/core';
+import {A6sRailwayResolverRegistry, A6sRailwayStationHandlersRegistry} from '../../lib/A6sRailway';
+import {StationContext} from '../../lib/models';
+import {StationException} from '../../lib/exception';
+
+export class MockA6sRailwayExternalFileStationHandler extends A6s_Railway_ExternalFile_StationHandler {
+    async run(
+        options: any,
+        handlers: A6sRailwayStationHandlersRegistry,
+        resolvers: A6sRailwayResolverRegistry,
+        stationContext: StationContext,
+    ): Promise<void> {
+        try {
+            await super.run(options, handlers, resolvers, stationContext);
+        } catch (e) {
+            if (e instanceof StationException) {
+                e.message = 'ENOENT: no such file or directory';
+            }
+
+            throw e;
+        }
+    }
+}
