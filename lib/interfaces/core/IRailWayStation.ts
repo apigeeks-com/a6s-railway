37d500604010196611f6af5a8a3be8eeabf2e68d
Igor Ognichenko <igor.ognichenko@fireblink.com>
2018-08-28 15:19:35 +0300

    add description

--- lib/interfaces/core/IRailWayStation.ts
+++ lib/interfaces/core/IRailWayStation.ts
@@ -2,10 +2,12 @@ import {IRailWayResolver, IRailWayResolver_JOI_SCHEMA} from './IRailWayResolver'
 import * as Joi from 'joi';
 
 interface IRailWayStation extends IRailWayResolver {
+    description?: string;
     resolvers?: {[key: string]: IRailWayResolver};
 }
 
 const IRailWayStation_JOI_SCHEMA = IRailWayResolver_JOI_SCHEMA.keys({
+    description: Joi.string(),
     resolvers: Joi.object().pattern(/^/, IRailWayResolver_JOI_SCHEMA)
 }).options({ abortEarly: true });
 
