006b1a034ca727c2d8b9e857f65dd0179c9b1901
Vlad Tkachenko <vlad@fireblink.com>
2019-03-25 14:20:37 +0200

    fix: helm install now uses optional version property

--- lib/utils/K8sHelmUtil.ts
+++ lib/utils/K8sHelmUtil.ts
@@ -71,6 +71,10 @@ export class K8sHelmUtil {
             cmd.push('--timeout ' + config.timeout);
         }
 
+        if (config.hasOwnProperty('version')) {
+            cmd.push('--version ' + config.version);
+        }
+
         if (config.wait) {
             cmd.push('--wait');
         }
